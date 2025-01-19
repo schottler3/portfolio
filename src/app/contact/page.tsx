c1b703a49e959d882fb5c73245ac44ee3359d2dc
root <root@AnimeMachine.>
2025-01-19 12:13:55 -0600

    Contact initial

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React, { useState } from 'react';
+import Header from '../components/Header';
+
+export default function Contact() {
+
+    return (
+        <div className="bg-charcoal min-h-screen">
+            <Header />
+        </div>
+    );
+}
