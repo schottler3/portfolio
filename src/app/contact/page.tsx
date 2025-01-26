cb7ee1665c87458d58510a12f92a04a8581fed6d
schottler3 <lucasschottler@gmail.com>
2025-01-25 23:02:36 -0600

    Bubbles and contact responsitivity

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
-import Coin from '../components/Coin';
+import Bubble from '../components/Bubble';
 import { addMail } from '../DataBasing';
 
 export default function Contact() {
@@ -129,11 +129,11 @@ export default function Contact() {
     };
 
     return (
-        <div className="bg-charcoal min-h-screen overflow-y-auto max-h-[100vh]">
+        <div className="bg-charcoal min-h-screen overflow-y-auto overflow-x-hidden max-h-[100vh]">
             <Header />
-            <div className="flex flex-col w-full mt-48 mb-[8vh]">
-                <div className="flex flex-col items-center *:justify-around *:select-none w-2/3 mx-auto *:text-white">
-                    <div className="mx-auto flex items-center justify-center varela-round-regular text-xl sm:text-2xl md:text-4xl lg:text-6xl xl:text-7xl text-center w-fit">
+            <div className="flex flex-col w-full mt-48">
+                <div className="flex flex-col items-center *:justify-around *:select-none w-full mx-auto *:text-white">
+                    <div className="flex items-center justify-center varela-round-regular text-3xl md:text-4xl lg:text-6xl xl:text-7xl text-center w-full">
                         <p id="displayText">{displayText}
                             <span id="cursor" className="inline-block text-aqua1">
                                 |
@@ -141,22 +141,60 @@ export default function Contact() {
                         </p>
                     </div>
 
-                    <div className="flex flex-col items-center *:justify-center *:space-x-24 *:select-none w-full *:bubble">
-                        <nav className="flex items-center h-full *:hover:cursor-pointer w-full mt-16">
-                            <Coin 
-                                front="GitHub" 
-                                back="schottler3" 
-                                link="https://github.com/schottler3"
-                            />
-                            <div className="ml-16 mr-16 mb-16 -mt-8">
-                                <Coin 
+                    <div className="flex flex-col w-full *:select-none *:bubble">
+                        <nav className="flex h-[40vh] *:hover:cursor-pointer mt-24 relative mb-4">
+                            <div className="absolute left-[40%] z-20">
+                                <Bubble 
+                                    front="GitHub" 
+                                    back="schottler3" 
+                                    link="https://github.com/schottler3"
+                                />
+                            </div>
+                            {/* Smallest top left */}
+                            <div className="absolute left-[42%] top-[30%] z-0">
+                                <Bubble
+                                    bubbleSize={7}
+                                />
+                            </div>
+                            {/* Top right */}
+                            <div className="absolute left-[70%] top-[-10%] z-0">
+                                <Bubble
+                                    bubbleSize={5}
+                                />
+                            </div>
+                            {/* Right middle*/}
+                            <div className="absolute left-[80%] top-[80%] z-0">
+                                <Bubble
+                                    bubbleSize={4}
+                                />
+                            </div>
+                            <div className="absolute left-[12%] top-[40%] z-20">
+                                <Bubble 
                                     front="LinkedIn" 
                                     back="Lucas Schottler" 
                                     link="https://www.linkedin.com/in/lucasschottler/" 
                                 />
                             </div>
-                            <div className="-mb-8">
-                                <Coin 
+                            {/* Large left top */}
+                            <div className="absolute left-[12%] top-[-10%] z-0">
+                                <Bubble
+                                    bubbleSize={10}
+                                />
+                            </div>
+                            {/* Smallest top left */}
+                            <div className="absolute left-[10%] top-[-20%] z-0">
+                                <Bubble
+                                    bubbleSize={2}
+                                />
+                            </div>
+                            {/* Smaller top left */}
+                            <div className="absolute left-[5%] top-[-10%] z-0">
+                                <Bubble
+                                    bubbleSize={3}
+                                />
+                            </div>
+                            <div className="absolute left-[60%] top-[10%] z-20">
+                                <Bubble 
                                     front="Handshake" 
                                     back="Lucas Schottler" 
                                     link="https://app.joinhandshake.com/profiles/73wng5" 
@@ -164,40 +202,51 @@ export default function Contact() {
                             </div>
                         </nav>
 
-                        <nav className="flex items-center h-full *:hover:cursor-pointer w-2/3">
-                            <div className="-mt-64">
-                                <Coin 
+                        <nav className="flex h-64 *:hover:cursor-pointer relative">
+                            {/* Below linkedIn on right  */}
+                            <div className="absolute left-[10%] md:left-[30%] top-0 z-0">
+                                <Bubble
+                                    bubbleSize={6}
+                                />
+                            </div>
+                            <div className="absolute left-[20%] bottom-16 z-20">
+                                <Bubble 
                                     front="Phone" 
                                     back="763-229-5934" 
                                     link="tel:1-763-229-5934" 
                                 />
                             </div>
-                            <div className="-ml-64">
-                                <Coin 
+                            <div className="absolute left-[40%] md:left-[50%] -top-32 z-20">
+                                <Bubble 
                                     front="Email" 
                                     back="Lucas@LucasSchottler.dev" 
                                     link="mailto:Lucas@lucasschottler.dev" 
+                                />
+                            </div>
+                            {/* Bottom Right*/}
+                            <div className="absolute left-[70%] bottom-4 z-10">
+                                <Bubble
+                                    bubbleSize={12}
                                 />
                             </div>
                         </nav>
                     </div>
                 </div>
                 <div 
-    style={{ backgroundImage: 'url(/images/Cloud.svg)' }} 
-    className="mt-[16vh] w-3/4 flex flex-col text-charcoal text-2xl 
-              items-center space-y-2 text-center mx-auto 
-              bg-no-repeat bg-center p-64 bg-contain"
->           
-                    <p className="text-white text-center text-6xl font-bold pb-16">Contact Me Via Email</p>
+                    className="lg:bg-[url(/images/Cloud.svg)] flex flex-col text-charcoal text-2xl 
+                            items-center space-y-2 text-center w-3/4 mx-auto
+                            bg-no-repeat bg-center pt-32 pb-64 bg-contain"
+                    >           
+                    <p className="text-white text-center text-5xl font-bold pb-16">Got Mail?</p>
                     <p id="status" className={`text-blue1 ${status ? 'flex' : 'hidden'}`}>{status}</p>
-                    <div className="flex flex-row w-1/2 justify-center">
-                        <input className="w-1/2 p-2 mr-1 " type="text" id="name" placeholder="Name" />
-                        <input className="w-1/2 p-2 ml-1" type="text" id="email" placeholder="Email" />
+                    <div className="flex flex-col md:flex-row justify-center items-center space-y-2">
+                        <input className="md:w-1/2 p-2 md:mr-1 " type="text" id="name" placeholder="Name" />
+                        <input className="md:w-1/2 p-2 md:ml-1" type="text" id="email" placeholder="Email" />
                     </div>
-                    <textarea className="w-1/2 p-2" id="message" placeholder="Message"></textarea>
-                    <div className="w-full h-full flex items-center justify-center pt-16">
+                    <textarea className="md:w-1/2 p-2" id="message" placeholder="Message"></textarea>
+                    <div className="w-full h-full flex items-center justify-center pt-8">
                         <button 
-                            className="text-white justify-center bg-blue1 w-1/4 h-8 items-center rounded-full flex hover:bg-slate-700" 
+                            className="text-white justify-center bg-blue1 w-1/3 h-8 items-center rounded-full flex hover:bg-slate-700" 
                             onClick={() => handleSubmit()}
                         >
                             Submit
