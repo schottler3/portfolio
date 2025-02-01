b9a5016d3e0db9e3578c43dd7c11c9750e23f408
schottler3 <lucasschottler@gmail.com>
2025-02-01 11:21:28 -0600

    Snowflake z/Image overlap projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,33 +61,33 @@ export default function Home() {
         <div className="fixed bottom-0 bg-white w-full z-10 bg-center h-5"></div>
         <Header />
         <div className="overflow-hidden z-10">
-        {snowFlakes.map(flake => (
-          <div
-            onClick={() => handleSnowflakeClick(flake.id)}
-            onTransitionEnd={(e) => handleOpacityEnd(flake.id, e)}
-            key={flake.id}
-            className={`absolute snowflake hover:cursor-pointer ${flake.clicked ? 'clicked' : ''}`}
-            style={{
-              left: `${flake.x}vw`,
-              top: `${flake.y}vh`,
-              transform: `translateY(${flake.y}vh)`,
-              width: `${flake.size}px`,
-              height: `${flake.size}px`,
-              animation: `movement ${flake.fallDuration}s linear infinite`,
-              '--fall-duration': `${flake.fallDuration}s`
-            } as React.CSSProperties}
-          />
+          {snowFlakes.map(flake => (
+            <div
+              onClick={() => handleSnowflakeClick(flake.id)}
+              onTransitionEnd={(e) => handleOpacityEnd(flake.id, e)}
+              key={flake.id}
+              className={`absolute snowflake z-10 hover:cursor-pointer ${flake.clicked ? 'clicked' : ''}`}
+              style={{
+                left: `${flake.x}vw`,
+                top: `${flake.y}vh`,
+                transform: `translateY(${flake.y}vh)`,
+                width: `${flake.size}px`,
+                height: `${flake.size}px`,
+                animation: `movement ${flake.fallDuration}s linear infinite`,
+                '--fall-duration': `${flake.fallDuration}s`
+              } as React.CSSProperties}
+            />
           ))}
         </div>
-        <div className="w-full">
+        <div className="w-full z-40 relative">
           <div className="pl-[10vw] pt-[32vh]">
-            <h1 className="text-aqua1 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold duration-500">
+            <h1 className="text-aqua1 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold duration-500 z-40">
               Welcome to
             </h1>
-            <h1 className="text-aqua1 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold pt-2 duration-500">
-              schottler3.github.io
+            <h1 className="text-aqua1 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold pt-2 duration-500 z-40">
+              LucasSchottler.dev
             </h1>
-            <h1 className="text-blue1 text-lg sm:text-xl md:text-2xl lg:text-3xl pt-10 duration-500">
+            <h1 className="text-blue1 text-lg sm:text-xl md:text-2xl lg:text-3xl pt-10 duration-500 z-40">
               A portfolio site by Lucas Schottler
             </h1>
           </div>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -40,7 +40,7 @@ export default function Projects() {
                 <div className="bg-[rgb(50,61,60)] h-full overflow-y-auto">
                     <div className="flex flex-col h-full pt-6 sm:pt-32">
                         {selectedProject ? (
-                            <div className="p-4 items-center text-center text-white h-full">
+                            <div className="p-4 items-center text-center text-white h-max">
                                 <p className="text-4xl font-bold">{selectedProject.title}</p>
                                 <p className="text-xl font-thin">{selectedProject.tagline}</p>
                                 <div className="w-full flex flex-row justify-center items-center gap-2 text-aqua1 font-thin">
@@ -56,34 +56,36 @@ export default function Projects() {
                                 </div>
                             </div>
                         ) : null}
-                        {selectedProject && selectedProject.images && (
-                            <div className="pb-8">
-                                {selectedProject.images.map((image, index) => (
-                                    <div key={index} className="p-4 flex justify-center">
-                                        <img src={image} alt={selectedProject.title} className="max-w-[90%]"/>
-                                    </div>
-                                ))}
+                        <div className="mt-8">
+                            {selectedProject && selectedProject.images && (
+                                <div className="pb-8">
+                                    {selectedProject.images.map((image, index) => (
+                                        <div key={index} className="p-4 flex justify-center">
+                                            <img src={image} alt={selectedProject.title} className="max-w-[90%]"/>
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
+                            {selectedProject && selectedProject.video && (
+                                <div className="p-4 flex flex-row justify-center items-center">
+                                {selectedProject.video.map((videoId, index) => {
+                                    const embedUrl = `https://www.youtube-nocookie.com/embed/${videoId}?rel=0&modestbranding=1`;
+                                    
+                                    return (
+                                        <div className="w-full max-w-4xl aspect-video" key={index}>
+                                            <iframe
+                                                className="w-full h-full"
+                                                src={embedUrl}
+                                                title="YouTube video player"
+                                                allow="picture-in-picture"
+                                                allowFullScreen
+                                            />
+                                        </div>
+                                    );
+                                })}
                             </div>
-                        )}
-                        {selectedProject && selectedProject.video && (
-                            <div className="p-4 flex flex-row justify-center items-center">
-                            {selectedProject.video.map((videoId, index) => {
-                                const embedUrl = `https://www.youtube-nocookie.com/embed/${videoId}?rel=0&modestbranding=1`;
-                                
-                                return (
-                                    <div className="w-full max-w-4xl aspect-video" key={index}>
-                                        <iframe
-                                            className="w-full h-full"
-                                            src={embedUrl}
-                                            title="YouTube video player"
-                                            allow="picture-in-picture"
-                                            allowFullScreen
-                                        />
-                                    </div>
-                                );
-                            })}
+                            )}
                         </div>
-                        )}
                     </div>
                 </div>
             </div>
