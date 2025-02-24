6c20e151d5424b4fe227e7844bbf6af545a170e4
schottler3 <lucasschottler@gmail.com>
2025-02-24 12:11:52 -0600

    build fix

--- src/app/storage.tsx
+++ src/app/storage.tsx
@@ -1,36 +1,17 @@
-import { initializeApp } from "firebase/app";
-import { getStorage, ref, getDownloadURL } from "firebase/storage";
-import { get } from "http";
-import { getURL } from "next/dist/shared/lib/utils";
-
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
-const firebaseConfig = {
-  // ...
-  storageBucket: 'schottler3.firebasestorage.app'
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-
-// Initialize Cloud Storage and get a reference to the service
-const storage = getStorage(app);
-
-// Create a storage reference from our storage service
-const storageRef = ref(storage);
+import { ref, getDownloadURL } from "firebase/storage";
+import { storage } from "./firebase";
 
 async function getImageUrl(imagePath: string) {
     // Create a reference to the specific image
     const imageRef = ref(storage, imagePath);
     
     try {
-      // Get the download URL
-      const url = await getDownloadURL(imageRef);
-      return url;
+        // Get the download URL
+        const url = await getDownloadURL(imageRef);
+        return url;
     } catch (error) {
-      console.error("Error getting download URL: ", error);
-      throw error;
+        console.error("Error getting download URL: ", error);
+        throw error;
     }
 }
 
