06be8ec45d0fe4b6459712c892f38461abfc8d60
schottler3 <lucasschottler@gmail.com>
2025-03-05 21:02:56 -0600

    Color pick on grid

--- src/app/projects/pathFinder/components/cell.tsx
+++ src/app/projects/pathFinder/components/cell.tsx
@@ -20,6 +20,19 @@ export default function Cell({cords, state}: CellItemProps) {
             className={`aspect-square border border-charcoal ${colors[state as keyof typeof colors]}`}
             data-x={cords[0]}
             data-y={cords[1]}
+            onClick={(e) => {
+                const target = e.target as HTMLDivElement;
+                const x = target.getAttribute("data-x");
+                const y = target.getAttribute("data-y");
+                if (x && y) {
+                    console.log(`Clicked on cell: ${x}, ${y}`);
+                    state += 1;
+                    if (state > 3) {
+                        state = 0;
+                    }
+                    target.className = `aspect-square border border-charcoal ${colors[state as keyof typeof colors]}`;
+                }
+            }}
         />
     );
 
