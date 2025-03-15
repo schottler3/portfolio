cfcabddbfd98f8a2bb7ae6dfdec5b19699c58c97
schottler3 <lucasschottler@gmail.com>
2025-03-15 10:45:18 -0500

    Mobile projectItem vertical spacing

--- src/app/components/ProjectItem.tsx
+++ src/app/components/ProjectItem.tsx
@@ -8,8 +8,8 @@ interface ProjectItemProps {
 export default function ProjectItem({title}: ProjectItemProps) {
 
     return (
-        <div className="w-full h-full flex flex-col items-center text-center justify-around text-white border-blue1 bg-navy border-4 p-4">
-            <div className="text-xl font-bold">{title}</div>
+        <div className="w-full h-full flex flex-col items-center justify-around text-white border-blue1 bg-navy border-4 pl-4 pr-4">
+            <div className="text-xl font-bold text-center">{title}</div>
         </div>
     );
 }
